599063eac981dbc93ec530042a683767a20b8572
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2024-10-08 21:46:46 +0900

    Feat. ButtonClass 기본 틀 제작

--- Game2/Button.h
+++ Game2/Button.h
@@ -1,5 +1,19 @@
 #pragma once
+class ObRect;
+class ObImage;
+
 class Button
 {
+public:
+	Button();
+	virtual ~Button();
+	virtual void Init();
+	virtual void Release();
+	virtual void Update();
+	virtual void LateUpdate();
+	virtual void Render();
+private:
+	ObRect* col;
+	ObImage* buttonIm;
 };
 
