ec350c47ff18502db2420425a7ce4a3129c139d5
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2024-10-19 01:22:58 +0900

    Feat. SoundClass 수정
    Feat. SoundClass 수정

---
+++ 2DFrameWork/Texture.h
@@ -0,0 +1,23 @@
+#pragma once
+class Texture :public Singleton<Texture>
+{
+private:
+    struct TextureNode
+    {
+        ID3D11ShaderResourceView*   srv; //ÀÚ¿ø
+        Int2                        textureSize;
+        ScratchImage* image;
+    };
+
+    //    first     second
+    //    key       value
+    map<wstring, TextureNode> textureList;
+
+public:
+    ~Texture();
+    ID3D11ShaderResourceView* LoadTexture(wstring file);
+    bool DeleteTexture(wstring file);
+    Int2 GetTextureSize(wstring file);
+    TextureNode GetTexture(wstring file);
+};
+
